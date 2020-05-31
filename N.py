a7893e7976095ce5f5ed964b3523b5e162b78740
DNedilko <55350009+DNedilko@users.noreply.github.com>
2020-05-31 21:51:47 +0300

    Update N.py

--- N.py
+++ N.py
@@ -1,23 +1,23 @@
 def shell(list):
-    interval=len(list)//2
+    interval=len(list)//2 \\визначається інтервал між елементами, що ми переглядаємо
     while interval>0:
-        for i in range(interval, len(list)):
-            t=int(list[i])
+        for i in range(interval, len(list)): 
+            t=int(list[i])    \\задається елемент для якого ми шукаємо місце
             j=i
-            while j>=interval and int(list[j-interval])>t:
-                list[j]=list[j-interval]
-                j-=interval
+            while j>=interval and int(list[j-interval])>t:\\порівнюється з тим, що стоїть на відстані інтервалу лівіше
+                list[j]=list[j-interval]  \\якщо елемент лівіше більший, то менший стає на місце більшогоб а індекс зменшується на значення інтервалу
+                j-=interval         \\так, поки виконується умова, що обраний індекс більший за кількість елементів в інтервалі та обраний елемент є меншим за той, що знаходиться на інтервальну відстань лівіше
             list[j]=t
-        interval//=2
+        interval//=2   \\дії відбувається, поки інтервал не зменшиться до 0(коли інтервал 1 то сортування стає звичайним сотруванням вставками)
     return list
 
 
 
-def insertion(list):
-    for i in range(len(list)):
+def insertion(list):    
+    for i in range(len(list)):     \\для кожного елементу з інтексом і
         j = i - 1
         elem = int(list[i])
-        while int(list[j]) > elem and j >= 0:
+        while int(list[j]) > elem and j >= 0:   відбувається порівняння з наступним та якщо цей елемент менший, то позиція не змінюється, якщо більший, то елемент переходить уліво
             list[j + 1] = list[j]
             j -= 1
         list[j + 1] = elem
